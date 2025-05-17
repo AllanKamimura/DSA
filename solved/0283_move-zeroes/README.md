8776bf17d57cd7d29bd7e1a4bd3e085e1021824c
Allan Henrique Kamimura <usp.br@usp.br>
2025-05-17 18:24:56 -0300

    0283: improve solution and explanation
    Signed-off-by: Allan Henrique Kamimura <usp.br@usp.br>

--- solved/0283_move-zeroes/README.md
+++ solved/0283_move-zeroes/README.md
@@ -29,5 +29,17 @@ i = 0 # -> first = 0
 j = 0 # -> second = 0
 ```
 
+#### Explanation
+
 While the first pointer keep track of the zeros, the second pointer looks for a non-zero value to swap.
 
+#### Time Complexity
+
+- O(n) -> In the worst case scenario,
+    - The number of swaps is n/2
+    - Consequently, the number of i steps is also n/2
+    - j takes n steps
+
+#### Space Complexity
+
+- O(1) -> We only create integer variables
--- solved/0283_move-zeroes/main.py
+++ solved/0283_move-zeroes/main.py
@@ -2,14 +2,13 @@ from typing import List
 
 
 class Solution:
-    def moveZeroes(self, nums: List[int]) -> None:
+    def moveZeroes(self, nums: List[int]) -> List[int]:
         """
         Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
 
         Note that you must do this in-place without making a copy of the array.
-        Do not return anything, modify nums in-place instead.
+        Modify nums in-place instead.
         """
-        print(nums)
         i = 0
         j = 0
 
@@ -17,11 +16,11 @@ class Solution:
             first = nums[i]
             second = nums[j]
 
-            if (first == 0) and (second == 0):
-                j += 1
-            elif second != 0:
+            if second != 0:
                 nums[i] = second
                 nums[j] = first
 
                 i += 1
-                j = i
+            j += 1
+
+        return nums
