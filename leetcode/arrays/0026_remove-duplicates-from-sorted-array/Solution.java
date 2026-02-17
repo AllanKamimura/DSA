f7a47ec4f3e2a7fc29392275c1732e56de263414
Allan Henrique Kamimura <usp.br@usp.br>
2026-02-17 00:31:39 -0300

    0026: add solved java
    Signed-off-by: Allan Henrique Kamimura <usp.br@usp.br>

---
+++ leetcode/arrays/0026_remove-duplicates-from-sorted-array/Solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int left = 0;
+
+        for (int right=0; right<nums.length; right++) {
+            if (nums[left] != nums[right]) {
+                left++;
+                nums[left] = nums[right];
+            }
+        }
+
+        left++;
+
+        return left;
+    }
+}
